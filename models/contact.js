8f565347c2f08670e1ab7e8e3dc02f2c86f5f106
javadoh <javadoh@gmail.com>
2023-10-16 20:31:07 -0300

    test

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose'), 
-Schema = mongoose.Schema;
-//,autoIncrement = require('mongoose-easy-auto-increment');//FUNCIONANDO OK
+Schema = mongoose.Schema,
+autoIncrement = require('mongoose-auto-increment');//FUNCIONANDO OK
+
+//var connection = mongoose.createConnection("mongodb://localhost/myDatabase");
+//autoIncrement.initialize(connection);
 
 mongoose.set('debug', true);
 
@@ -16,7 +19,6 @@ var ContactData = new Schema({
 
 console.log("Está en el model");
 
-//ContactData.plugin(autoIncrement, {field: 'id', collection: 'webmsg_counter'});//FUNCIONA EXCELENTE
-
+ContactData.plugin(autoIncrement.plugin, {model: 'webmsg_counter', field: 'id'});//FUNCIONA EXCELENTE
 var ContactData = mongoose.model('ContactData', ContactData, 'webmsg');
 module.exports = ContactData;
